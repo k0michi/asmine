1f6cc1ec27b06cfb925b1d3d48f273df0b82c209
k0michi <k0michi@koyomi.co>
2025-05-22 21:31:00 +0900

    Change project name

--- src/main/java/com/koyomiji/jasmine/regex/compiler/string/StringRegexes.java
+++
@@ -1,7 +0,0 @@
-package com.koyomiji.jasmine.regex.compiler.string;
-
-public class StringRegexes {
-  public static CharLiteralNode literal(char literal) {
-    return new CharLiteralNode(literal);
-  }
-}
